c4087d68c9a4e2a8008d17e4c394ad533b68e59d
Ashim Shrestha <ashim@jankaritech.com>
2025-10-08 10:48:07 +0545

    chore: make all docker compose services work with HTTPS
    Signed-off-by: Ashim Shrestha <ashim@jankaritech.com>

--- e2e/tests/config.ts
+++ e2e/tests/config.ts
@@ -11,7 +11,7 @@ export const config = {
   PASSWORD: process.env.PASSWORD ?? 'testtest',
   USER_EMAIL: process.env.USER_EMAIL ?? 'alice@example.com',
   INSTANCE_URL: (process.env.INSTANCE_URL ?? 'https://localhost:8443').replace(/\/+$/, ''),
-  CONTROLLER_HOST: (process.env.CONTROLLER_HOST ?? 'http://localhost:11311').replace(/\/+$/, ''),
+  CONTROLLER_HOST: (process.env.CONTROLLER_HOST ?? 'https://localhost:21311').replace(/\/+$/, ''),
   browser: process.env.BROWSER ?? 'chromium',
   headless: process.env.HEADLESS === 'true',
   slowMo: Number(process.env.SLOW_MO) || 0,
