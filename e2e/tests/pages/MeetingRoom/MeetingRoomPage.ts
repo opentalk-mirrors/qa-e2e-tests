4b8bfca9c6b2a0188a9dc23ed93719f9b07851e7
Ashim Shrestha <ashim@jankaritech.com>
2026-04-29 17:15:29 +0545

    test: update locator
    Signed-off-by: Ashim Shrestha <ashim@jankaritech.com>

--- e2e/tests/pages/MeetingRoom/MeetingRoomPage.ts
+++ e2e/tests/pages/MeetingRoom/MeetingRoomPage.ts
@@ -200,7 +200,9 @@ export class MeetingRoomPage {
     this.chatHistoryDescription = this.page.getByRole('tabpanel', { name: 'Chat' }).getByRole('paragraph');
     this.joinedText = this.page.locator('//*[@data-testid="user-event-message"]');
     this.chatListItems = this.page.getByRole('tabpanel', { name: 'Chat' }).getByRole('listitem');
-    this.noMessageMatchText = this.page.locator('//*[@data-sentry-component="NoSearchResult"]').locator('span');
+    this.noMessageMatchText = this.page
+      .getByRole('tabpanel', { name: 'Chat' })
+      .getByText('No messages matching the criteria');
     this.resetButton = this.page.getByRole('button', { name: 'Reset' });
     this.emojiPicker = this.page.getByRole('button', { name: 'open emoji picker' });
     this.chatTextField = this.page.getByPlaceholder('Type a message');
